968016f206bd7aa41029eb7c002d2892723463dc
Juan Pablo Nahuelpán <juanpablonahuelpan@gmail.com>
2021-06-30 20:38:31 -0400

    ed(3)

---
+++ bash_aliases.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# bash commands
+alias nf="touch"  # new file => nf
+alias vf="cat"    # view file => vf
+alias cdp="cd .." # cd parent => cdp
+
+#  git commands 
+alias gits="git status"
+alias gitl="git log"
+alias gitc="git commit -m"
+alias gitpush = "git push -u origin main"
