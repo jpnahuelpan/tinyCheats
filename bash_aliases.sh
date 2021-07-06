49cbe45c72c6bb5d51c76dceed1a4c114a74046e
Juan Pablo Nahuelpán <juanpablonahuelpan@gmail.com>
2021-07-06 02:26:11 -0400

    ed(4)

--- bash_aliases.sh
+++ bash_aliases.sh
@@ -5,6 +5,9 @@ alias nf="touch"  # new file => nf
 alias vf="cat"    # view file => vf
 alias cdp="cd .." # cd parent => cdp
 
+# bash files
+alias ghtokens="vf $HOME/.tokens_file" # view yours tokens in tokens file.
+
 #  git commands 
 alias gits="git status"
 alias gitl="git log"
