3e23187c154b4fbfde8f2be926ef27cab1ff4de0
Juan Pablo Nahuelpán <nahuelpan.ide@gmail.com>
2021-08-27 21:50:01 -0400

    modificando skeleton.py_

--- skeleton.py.sh
+++ skeleton.py.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# este es un script para un skeleton para proyecto de base python.
+# este es un script para un skeleton para proyecto con base python.
 
 # ingresar donde se quiere crear el proyecto.
 dp=$1 # directory project(dp).
@@ -14,27 +14,30 @@ mkdir $dp/docs
 mkdir $dp/templates
 
 # paso 2 crear archivos relevantes.
-touch $dp/tests/__init__.py
-touch $dp/$dp/__init__.py
 echo -e "#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 try:
     from setuptools import setup\n
 except ImportError:
     from distutils.core import setup\n
-config = [
-    'description': '',
-    'author': 'Juan Pablo Nahuelpán',
-    'url': 'github.com/jpnahuelpan/$dp',
-    'download_url': '',
-    'author_email': 'juanpablonahuelpan@mail.com',
-    'version': '0.1',
-    'install_requires': [''],
-    'packages': [''],
-    'scripts': [],
-    'name': '$dp'
-]\n
-setup(**config)" >> $dp/setup.py 
- 
+setup(name='$dp',
+      version='1.0.0',
+      description='',
+      author='Juan Pablo Nahuelpán',
+      author_email='juanpablonahuelpan@gmail.com',
+      url='',
+      packages=['distutils'],
+)\n
+" >> $dp/setup.py 
+
+echo -e "#!/usr/bin/env python
+# -*- coding: utf-8 -*-" >> $dp/bin/app.py 
+
+touch $dp/$dp/__init__.py
+touch $dp/tests/__init__.py
+
 echo -e "#!/usr/bin/env python
 # -*- coding: utf-8 -*-" >> $dp/tests/$dp\_tests.py 
+
+# paso 3 crear enviroment.
+virtualenv venv 
